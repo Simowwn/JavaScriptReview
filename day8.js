613df2b6b516df22270c84dad2dbdc8a901fee7c
Simowwn <125945116+Simowwn@users.noreply.github.com>
2025-08-02 20:06:10 +0800

    day 8- 10

--- day8.js
+++ day8.js
@@ -1,12 +1,18 @@
 let name;
 let age;
 {
-let height;
-			// 2
- { 					// 2
- { let weight; 		// 1 	// 2
- console.log(name); 	// 1 	// 2
- 					// 2
-   console.log(name); 		// 2
+  let height;
+  // 2
+  {
+    // 2
+    {
+      let weight; // 1 	// 2
+      console.log(name); // 1 	// 2
+      // 2
+      console.log(name); // 2
+    }
+    console.log(age); // 1 	// 2
+    console.log(height); // 1 	// 2
+    console.log(weight); // 1 	// 2
+  }
 }
-6
---
+++ day9.js
@@ -0,0 +1,28 @@
+console.log("Arithmetic operators");
+console.log(2 * 3 + 1); // expected 7
+console.log(2 ** 4); // expected 16
+console.log(5 / 1); // expected 5
+console.log(8 ** 2 - 5 ** 2); // expected 39
+
+console.log("\nComparison operators");
+// Exercise 2: Fill in the missing comparison operators in that such a way that all expressions result in true
+// (replace the underscore symbol with the appropriate operator):
+console.log(4 * 5 == 20);
+console.log(6 * 5 == "30");
+console.log(-17 < 0);
+console.log(25 > 1);
+console.log(2 + 2 * 2 != 4);
+
+console.log("\nLogical operators");
+// Exercise 3: Fill in the missing comparison operators in that such a way that all expressions result in true
+// (replace the underscore symbol with the appropriate operator):
+
+// a conjunction, i.e. logical AND (&& symbol)
+// an alternative, i.e. logical OR (symbol ||)
+// a negation, i.e. logical NOT (symbol !)
+
+console.log(true || false);
+console.log(false || !false);
+console.log(false || false || true);
+console.log(true || (false && false) || true);
+console.log(true || (false && false && true));

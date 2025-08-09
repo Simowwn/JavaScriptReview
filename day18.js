9e190fd66492131684c31517ddbaa4d7443abc5b
Simowwn <125945116+Simowwn@users.noreply.github.com>
2025-08-09 22:58:29 +0800

    Fibonacci

--- day18.js
+++ day18.js
@@ -58,3 +58,75 @@ console.log(action(addd, num1, num2));
 // CALLBACK IS OVERKILL IN THIS EXAAMPLE
 
 // exercise 5 tomorrow
+// Exercise 5: Write a program that will print out (to the console) consecutive integers 10 times,
+// in two-second intervals (start with the number 1). Use the functions setInterval, clearInterval and setTimeout.
+
+// let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// for (let i = 0; i < nums.length; i++) {
+//   setInterval(() => {
+//     console.log(nums[i]);
+//   }, 2000);
+// }
+
+// setTimeout(() => {
+//   clearInterval();
+// }, 2000 * nums.length);
+
+// let count = 1;
+
+// let intervalID = setInterval(() => {
+//   console.log(count);
+//   count++;
+
+//   if (count > 10) {
+//     clearInterval(intervalID);
+//   }
+// }, 2000);
+
+// So each element of the sequence (except the first two) is the sum of the previous two.
+// For example: F1 = 1, F2 = F1 + F0 = 1, F3 = F2 + F1 = 2 and F6 = F5 + F4 = 8. The function should use recursion. In the definition, use a function expression
+//  (store an anonymous function in a variable).
+// Exercise 6: Write a function that will return the nth Fibonacci number.
+
+function fibonacci(n) {
+  return n <= 1 ? n : fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+console.log(fibonacci(6)); // Output: 8
+
+let fibbRec = function (n) {
+  let retVal = 0;
+  if (n != 0) {
+    if (n === 1) {
+      retVal = 1;
+    } else {
+      retVal = fibbRec(n - 1) + fibbRec(n - 2);
+    }
+  }
+  return retVal;
+};
+
+function fibbi(n) {
+  if (n <= 1) return n;
+  return fibbi(n - 1) + fibbi(n - 2);
+}
+
+function printFibbi(limit) {
+  for (let i = 0; i <= limit; i++) {
+    console.log(fibbi(i));
+  }
+}
+printFibbi(6); // Output: 0, 1, 1, 2, 3, 5, 8
+
+function fibbiItter(n) {
+  let a = 0,
+    b = 1;
+
+  for (let i = 2; i <= n; i++) {
+    let c = a; //0 , 1  1  2
+    a = b; //1 , 1  2  3
+    b += c; //1 , 2  3  5
+  }
+  return b;
+}
